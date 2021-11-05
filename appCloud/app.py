e8b8d747880bc45040cf25ef51c3d6d5e43eeb39
ampacheco <ampacheco@users.noreply.github.com>
2021-11-05 18:57:27 -0400

    step-02

--- appCloud/app.py
+++ appCloud/app.py
@@ -8,3 +8,10 @@ st.title('DataFrame Demo')
 ## Elements on the sidebar
 #st.sidebar.selectbox('Choose a demo', ['DataFrame Demo', 'Other'])
 #check=st.sidebar.checkbox('Show code')
+
+#write some text describing the app
+'''
+This is a demo shows how to use `st.write` to visualize Pandas DataFrames
+
+[Data courtesy of the [UN Data Explorer](http://data.un.org/Data.aspx?d=FAO&f=itemCode%3a2051).)
+'''
