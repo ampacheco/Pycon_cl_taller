46cb58c02e7ef4330eeabf0e4b5c38b88749c14c
ampacheco <ampacheco@users.noreply.github.com>
2021-11-05 19:29:41 -0400

    test

--- appCloud/app.py
+++ appCloud/app.py
@@ -30,3 +30,9 @@ col1,col2,col3 = st.columns([5,1,5])
 with col1.expander('widgets'):
     year_range = st.slider('year range',year_min,year_max,[1800,1900],step=10)
     vals=st.multiselect('',cols)
+
+df1=df[(df['year']>=year_range[0] ) & (df['year']<=year_range[1] )]
+df2=df1[vals]
+
+with col1.expander('data'):
+    st.dataframe(df2)
