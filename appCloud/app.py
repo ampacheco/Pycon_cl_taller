35e0bea3ec4699d0a4866426fb856da117ded080
ampacheco <ampacheco@users.noreply.github.com>
2021-11-05 19:16:22 -0400

    lab-02-01

--- appCloud/app.py
+++ appCloud/app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 import pandas as pd
 
+st.title('AppMap')
+st.markdown('This is a simple app to help you visualize your app\'s data.')
+st.markdown('**Note:** This app is still in development. Please report any bugs or suggestions to [@napoles3D](https://twitter.com/napoles3D)')
