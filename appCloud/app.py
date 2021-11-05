bc08306c8ecb0d634ff3bc24b7e1affa13fdbb08
ampacheco <ampacheco@users.noreply.github.com>
2021-11-05 19:21:02 -0400

    fixing

--- appCloud/app.py
+++ appCloud/app.py
@@ -29,20 +29,3 @@ col1,col2,col3 = st.columns([5,1,5])
 with col1.expander('widgets'):
     year_range = st.slider('year range',year_min,year_max,[1800,1900],step=10)
     vals=st.multiselect('',cols)
-
-with col1.expander('data'):
-    st.dataframe(df2)
-
-if ("lat" in vals) and ("lon" in vals):
-    col3.map(df2)
-
-
-check=col1.checkbox('Save File')
-if check:
-    df2.to_csv('data.csv')
-    name=col1.text_input('*.csv file name:')
-    #check if name is not empty
-    if name:
-        with open('data.csv') as f:
-            col1.download_button('Download Data',f,file_name=name)
-
